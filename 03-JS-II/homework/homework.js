54acda43da0df1d5e272386bece35499103a9369
Yoss1800 <contacto2000@gmail.com>
2022-06-13 22:38:23 -0300

    final

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -156,12 +156,13 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
   if (numero === 0 || numero === 1) return false;
-  for (var i = 2 ; numero % i === 0 && i <= 9; i++)
-  if (numero % i !== 0) {
-    return true;
-  } else {
-    return false
+  if(numero === 2) return true;
+  for(var i = 2; i < numero; i++) {
+    if(numero % i === 0) {
+      return false;
+    }
   }
+  return true;
 }
 
 function esVerdadero(valor){
@@ -179,9 +180,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  for (var i = 0; i <= 10; i++) {
-    return (i * 6);
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
   }
+  return arrayTablaDel6
 }
 
 function tieneTresDigitos(numero){
